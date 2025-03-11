18ee062630ebae34affc8ed39e68903e6b64e8de
Martin Dilger <martin@nebulit.de>
2025-03-11 18:16:27 +0100

    draft

--- app/slices/cartitems/CartItems.tsx
+++ app/slices/cartitems/CartItems.tsx
@@ -7,19 +7,19 @@ import {CartEvents} from "@/app/api/events/CartEvents";
 
 export default function CartItems(props: {aggregateId: string}) {
 
-    const [cartItems, setCartItems] = useState<number[]>()
+    const [cartItems, setCartItems] = useState<CartItem[]>()
 
     useEffect(() => {
         subscribeStream(Streams.Cart, (nextExpectedStreamVersion:bigint, events:CartEvents[]) => {
-            alert(JSON.stringify(events))
-            setCartItems([1,2,3])
+            setCartItems(cartItemsStateView(events))
         })
-        setCartItems([1,2,3])
     }, []);
 
-    return (<div>
+    return (<div className={"content"}>
         <h3>Cart Items</h3>
-        {JSON.stringify(cartItems?.length)}
-        {JSON.stringify(cartItems)}
+        {cartItems?.map(item => <div>
+            <span>{item.name}</span>
+            <b className={"m-2 p-2"}>{item.price}</b>
+        </div>)}
     </div>)
 }
